694d85ff67f3f26777ec30153dcbf1f2538f5481
Saskianp <saskianovtarisaputri@gmail.com>
2025-02-09 23:54:04 +0700

    update

--- models/bookingType.go
+++ models/bookingType.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type BookingType struct {
 	ID          uint   `gorm:"primaryKey;autoIncrement" json:"id"`
 	BookingType string `gorm:"type:varchar(100);not null" json:"bookingType"`
-	Description int    `gorm:"type:varchar(100);not null" json:"description"`
+	Description string    `gorm:"type:varchar(100);not null" json:"description"`
 }
 
 func MigrateBookingType(db *gorm.DB) error {
